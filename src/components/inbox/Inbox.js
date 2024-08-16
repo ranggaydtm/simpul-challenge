e87f975f24a7ae364f708e37a5ae0ed02f4fd065
ranggaydtm <ranggay67@gmail.com>
2024-08-16 20:17:08 +0700

    fixing inbox

--- src/components/inbox/Inbox.js
+++ src/components/inbox/Inbox.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { PersonOutlineOutlined, Search } from "@mui/icons-material";
-import { Avatar, Badge, Box, CircularProgress, InputAdornment, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Modal, OutlinedInput, Stack, TextField, Typography } from "@mui/material";
+import { Avatar, Badge, Box, CircularProgress, InputAdornment, Modal, OutlinedInput, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getAllInbox } from "../../services";
 import { boxStyle, inboxStyle, nameStyle } from "../../theme";
@@ -60,7 +60,7 @@ function Inbox({ open, close }) {
                     {row?.type === "group" ? (
                       <>
                         <Stack key={index} direction="row" sx={inboxStyle}>
-                          <Stack direction="row" py={2} sx={{ bgcolor: "" }}>
+                          <Stack direction="row" py={2}>
                             <Avatar sx={{ display: "absolute" }}>
                               <PersonOutlineOutlined sx={{ color: "#4F4F4F" }} />
                             </Avatar>
@@ -68,7 +68,7 @@ function Inbox({ open, close }) {
                               <PersonOutlineOutlined sx={{ color: "white" }} />
                             </Avatar>
                           </Stack>
-                          <Stack direction="column" px={2} py={2} sx={{ bgcolor: "" }}>
+                          <Stack direction="column" px={2} py={2}>
                             <Stack direction="row" spacing={2}>
                               <Typography sx={nameStyle}>{row?.groupName}</Typography>
                               <Typography variant="caption" fontSize="14px" sx={{ minWidth: "120px", bgcolor: "" }}>
@@ -84,8 +84,8 @@ function Inbox({ open, close }) {
                           </Stack>
                           {!row?.read ? (
                             <>
-                              <Stack sx={{ bgcolor: "aliceblue", width: "10px", direction: "row", alignItems: "center" }}>
-                                <Badge badgeContent={4} variant="dot" sx={{ color: "#EB5757" }} />
+                              <Stack sx={{ width: "10px", direction: "row", alignItems: "center" }}>
+                                <Badge variant="dot" sx={{ color: "#EB5757" }} />
                               </Stack>
                             </>
                           ) : null}
@@ -94,7 +94,7 @@ function Inbox({ open, close }) {
                     ) : (
                       <>
                         <Stack key={index} direction="row" sx={inboxStyle}>
-                          <Stack direction="row" px="10px" py={2} sx={{ bgcolor: "" }}>
+                          <Stack direction="row" px="10px" py={2}>
                             <Avatar sx={{ bgcolor: "#2F80ED" }}>
                               <PersonOutlineOutlined sx={{ color: "white" }} />
                             </Avatar>
@@ -125,86 +125,6 @@ function Inbox({ open, close }) {
             </Stack>
           </>
         )}
-        {/* {!loading ? (
-          <>
-            {data &&
-              data.map((row, index) => {
-                const lastMessage = row.messages[row.messages.length - 1];
-                return (
-                  <>
-                    {row?.type === "group" ? (
-                      <>
-                        <List sx={{ backgroundColor: "", width: "100%" }}>
-                          <ListItem sx={{ width: "100%" }}>
-                            <ListItemButton>
-                              <ListItemIcon sx={{}}>
-                                <Avatar sx={{ display: "absolute" }}>
-                                  <PersonOutlineOutlined sx={{ color: "#4F4F4F" }} />
-                                </Avatar>
-                                <Avatar sx={{ bgcolor: "#2F80ED", ml: -2.5 }}>
-                                  <PersonOutlineOutlined sx={{ color: "white" }} />
-                                </Avatar>
-                              </ListItemIcon>
-                              <ListItemText
-                                primary={
-                                  <>
-                                    <Typography fontSize="16px" fontWeight={600} sx={{ color: "#2F80ED" }}>
-                                      {row?.groupName}
-                                    </Typography>
-                                  </>
-                                }
-                                secondary={
-                                  <>
-                                    <Typography variant="subtitle2" fontWeight="bold" sx={{}}>
-                                      {lastMessage?.sender}
-                                    </Typography>
-                                    <Typography variant="caption">{lastMessage?.content}</Typography>
-                                  </>
-                                }
-                              />
-                            </ListItemButton>
-                          </ListItem>
-                        </List>
-                      </>
-                    ) : (
-                      <>
-                        <List sx={{ backgroundColor: "", width: "100%" }}>
-                          <ListItem sx={{ bgcolor: "#f2f2" }}>
-                            <ListItemButton>
-                              <ListItemIcon sx={{ mr: 2 }}>
-                                <Avatar sx={{ bgcolor: "#2F80ED" }}>
-                                  <PersonOutlineOutlined sx={{ color: "white" }} />
-                                </Avatar>
-                              </ListItemIcon>
-                              <ListItemText
-                                primary={
-                                  <>
-                                    <Typography fontSize="16px" fontWeight={600} sx={{ color: "#2F80ED" }}>
-                                      {row?.user}
-                                    </Typography>
-                                  </>
-                                }
-                                secondary={<>{lastMessage?.content}</>}
-                              />
-                            </ListItemButton>
-                          </ListItem>
-                        </List>
-                      </>
-                    )}
-                  </>
-                );
-              })}
-          </>
-        ) : (
-          <>
-            <Stack direction="column" spacing={2} sx={{ backgroundColor: "", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-              <CircularProgress sx={{ color: "#828282" }} size="3rem" thickness="5" />
-              <Typography>Loading Chats ...</Typography>
-            </Stack>
-          </>
-        )} */}
-        {/* <Stack key={index} direction="row" paddingTop={2} sx={{ bgcolor: "", cursor: "pointer", borderBottom: "1px solid" }}>
-         </Stack> */}
       </Box>
     </Modal>
   );
