e4f99267bb1d3f3b3270b709fccb3c0e14e538bd
ranggaydtm <ranggay67@gmail.com>
2024-08-16 20:18:00 +0700

    fetch Task API

--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getAllInbox = async () => {
   return await axios.get(`https://private-d031dca-simplequicks.apiary-mock.com/inbox`);
 };
+
+export const getAllTask = async () => {
+  return await axios.get(`https://private-d031dca-simplequicks.apiary-mock.com/task`);
+};
